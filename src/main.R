f9f97654b972f855b9a8d9f34212648f5f38b856
Vincent Labatut <vince.labatut@gmail.com>
2018-12-01 17:18:04 +0100

    added plot script

--- src/main.R
+++ src/main.R
@@ -5,6 +5,10 @@
 # Vincent Labatut
 # 11/2018
 ###############################################################################
+library("TraMineR")
+library(cluster)
+
+
 CURRENT_YEAR <- 2018
 
 # init file paths
@@ -15,11 +19,23 @@ traj.file <- file.path(data.folder,"trajectories.txt")
 # read personal data table
 perso <- read.csv(perso.file, header=TRUE, check.names=FALSE)
 
+# function to convert statuses
+status.map <- c(
+	"cd"="Conseiller·e départemental·e",
+	"cm"="Conseiller·e municipal·e",
+	"cr"="Conseiller·e régional·e",
+	"dep"="Député·e",
+	"mai"="Maire",
+	"min"="Ministre",
+	"pm"="Premier ministre"
+)
+
 # read trajectories
 #old version
 #traj.table <- read.csv(traj.file, header=FALSE)
 con <- file(traj.file, open="r")
 temp <- readLines(con)
+close(con)
 cols <- strsplit(temp, split=',', fixed=TRUE)
 #traj <- list()
 #for(tmp in cols)
@@ -30,24 +46,105 @@ cols <- strsplit(temp, split=',', fixed=TRUE)
 #						position=e[3]))
 #	traj[[tmp[1]]] <- evt.lst
 #}
-traj.mat <- matrix(ncol=5,nrow=0)
-colnames(traj) <- c("id","index","from","to","status")
+traj.df <- data.frame(id=integer(), index=integer(), from=integer(), to=integer(), status=character(), stringsAsFactors=TRUE)
 for(tmp in cols)
 {	id <- tmp[1]
 	idx <- 0
 	evts <- strsplit(tmp[2:length(tmp)], split=':', fixed=TRUE)
-	evt.mat <- t(sapply(evts, function(e)
-		{	idx <<- idx + 1
-			c(id=id, index=idx, from=e[1], to=ifelse(e[2]=="hui", CURRENT_YEAR, e[2]), status=e[3])
-		}))
-	traj.mat <- rbind(traj,evt.mat)
+	for(evt in evts)
+	{	idx <- idx + 1
+		#print(evt)
+		df <- data.frame(id=as.integer(id), index=as.integer(idx), 
+				from=as.integer(evt[1]), to=as.integer(ifelse(evt[2]=="hui", CURRENT_YEAR, evt[2])), 
+				status=status.map[evt[3]], stringsAsFactors=TRUE)
+		traj.df <- rbind(traj.df, df)
+	}
 }
-traj <- data.frame(traj.mat)
 
-# convert to TraMineR format
-traj.labels <- sort(seqstatl(traj[,"status"]))	# full state names
-traj.states <- sort(seqstatl(traj[,"status"]))	# short state names
-traj.seq <- seqdef(traj, var=c("id", "from", "to", "status"), 
+# convert trajectories to TraMineR format
+traj.labels <- status.map			# full state names
+traj.states <- names(status.map)	# short state names
+traj.seq <- seqdef(traj.df, var=c("id", "from", "to", "status"), 
 		informat="SPELL", 
 		states=traj.states, labels=traj.labels,
-		process=FALSE)							# calendar time
+		process=FALSE)				# FALSE => calendar time
+
+# plot n°1
+plot.filename <- file.path(data.folder,"fig1.pdf")
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	par(mfrow=c(2,2))
+	seqiplot(traj.seq, main="First 10 sequences", with.legend=FALSE)
+	seqIplot(traj.seq, main="All sequences", sortv="from.start", with.legend=FALSE)
+	seqfplot(traj.seq, main="10 most frequent sequences", with.legend=FALSE, pbarw=TRUE)
+	seqlegend(traj.seq)
+dev.off()
+
+# plot n°2
+plot.filename <- file.path(data.folder,"fig2.pdf")
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	par(mfrow=c(2,2))
+	seqdplot(traj.seq, main="State distribution", with.legend=FALSE, border=NA)
+	seqHtplot(traj.seq, main="Entropy index")
+	seqmtplot(traj.seq, main="Mean time", with.legend=FALSE)
+	seqmsplot(traj.seq, main="Modal state sequence", with.legend=FALSE, border=NA)
+dev.off()
+
+# compute inter-sequence distances and clusters
+traj.dist <- seqdist(traj.seq, method="OM", indel=1, sm="TRATE", with.missing=TRUE)
+clusters <- agnes(traj.dist, diss=TRUE, method="ward")
+plot(clusters, which.plot=2)
+cl4 <- cutree(clusters, k=4)
+cl4.fac <- factor(cl4, labels=paste("Cluster", 1:4))
+
+# plot n°3: State distribution within each cluster
+plot.filename <- file.path(data.folder,"fig3.pdf")
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	seqdplot(traj.seq, group=cl4.fac, border=NA)
+dev.off()
+
+# plot n°4: Representative sequence of each cluster
+plot.filename <- file.path(data.folder,"fig4.pdf")
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	seqrplot(traj.seq, diss=traj.dist, group=cl4.fac, border=NA)
+dev.off()
+
+# compute discrepancy
+da <- dissassoc(traj.dist, group=perso[,"Sexe"], R=5000)
+print(da$stat)
+
+# plot n°5: Association between the state sequences and the selected covariate
+plot.filename <- file.path(data.folder,"fig5.pdf")
+gdiff <- seqdiff(traj.seq, group=perso[,"Sexe"], cmprange=c(0,5), 
+		seqdist.args=list(method="OM", indel=1, sm="TRATE"))
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	plot(gdiff, main="Evolution of the pseudo R2 and L", stat=c("Pseudo R2", "Levene"), lwd=2)
+dev.off()
+
+# plot n°6: Evolution of the within-group discrepancy
+plot.filename <- file.path(data.folder,"fig6.pdf")
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	plot(gdiff, main="Evolution of within-group and overall discrepancies", 
+			stat=c("discrepancy"), lwd=2, legend.pos="topright")
+dev.off()
+
+# plot n°7: Regression tree for the state sequences
+plot.filename <- file.path(data.folder,"fig7.png")
+st <- seqtree(traj.seq ~ Sexe + `Groupe politique` + Profession, data=perso,
+		R=5000, diss=traj.dist, pval = 0.05)
+seqtreedisplay(st, filename=plot.filename, type="d", border=NA, gvpath='D:/Program Files (x86)/Graphviz2.38')
+
+# plot n°8: Most frequent subsequences
+plot.filename <- file.path(data.folder,"fig8.pdf")
+traj.seqe <- seqecreate(traj.seq, use.labels=FALSE)
+fsubseq <- seqefsub(traj.seqe, pmin.support=0.05)
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	plot(fsubseq[1:15], col="cyan", main="15 most frequent subsequences")
+dev.off()
+
+# plot n°9: Subsequences of transitions in each cluster
+plot.filename <- file.path(data.folder,"fig9.pdf")
+discr <- seqecmpgroup(fsubseq, group=cl4.fac)
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	plot(discr[1:6])
+dev.off()
+
