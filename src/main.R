4b84d58cf0a0357b7f194427adf4a454a956e58d
Vincent Labatut <vince.labatut@gmail.com>
2018-12-05 17:12:54 +0100

    added a missing figure

--- src/main.R
+++ src/main.R
@@ -70,7 +70,7 @@ traj.seq <- seqdef(traj.df, var=c("id", "from", "to", "status"),
 		process=FALSE)				# FALSE => calendar time
 
 # plot n°1
-plot.filename <- file.path(data.folder,"fig1.pdf")
+plot.filename <- file.path(data.folder,"fig01.pdf")
 pdf(file=plot.filename,bg="white",compress=FALSE)
 	par(mfrow=c(2,2))
 	seqiplot(traj.seq, main="First 10 sequences", with.legend=FALSE)
@@ -80,7 +80,7 @@ pdf(file=plot.filename,bg="white",compress=FALSE)
 dev.off()
 
 # plot n°2
-plot.filename <- file.path(data.folder,"fig2.pdf")
+plot.filename <- file.path(data.folder,"fig02.pdf")
 pdf(file=plot.filename,bg="white",compress=FALSE)
 	par(mfrow=c(2,2))
 	seqdplot(traj.seq, main="State distribution", with.legend=FALSE, border=NA)
@@ -96,14 +96,20 @@ plot(clusters, which.plot=2)
 cl4 <- cutree(clusters, k=4)
 cl4.fac <- factor(cl4, labels=paste("Cluster", 1:4))
 
-# plot n°3: State distribution within each cluster
-plot.filename <- file.path(data.folder,"fig3.pdf")
+# plot n°3: Sequences within each cluster
+plot.filename <- file.path(data.folder,"fig03.pdf")
+pdf(file=plot.filename,bg="white",compress=FALSE)
+	seqIplot(traj.seq, group=cl4.fac, sortv="from.start")
+dev.off()
+
+# plot n°4: State distribution within each cluster
+plot.filename <- file.path(data.folder,"fig04.pdf")
 pdf(file=plot.filename,bg="white",compress=FALSE)
 	seqdplot(traj.seq, group=cl4.fac, border=NA)
 dev.off()
 
-# plot n°4: Representative sequence of each cluster
-plot.filename <- file.path(data.folder,"fig4.pdf")
+# plot n°5: Representative sequence of each cluster
+plot.filename <- file.path(data.folder,"fig05.pdf")
 pdf(file=plot.filename,bg="white",compress=FALSE)
 	seqrplot(traj.seq, diss=traj.dist, group=cl4.fac, border=NA)
 dev.off()
@@ -112,29 +118,29 @@ dev.off()
 da <- dissassoc(traj.dist, group=perso[,"Sexe"], R=5000)
 print(da$stat)
 
-# plot n°5: Association between the state sequences and the selected covariate
-plot.filename <- file.path(data.folder,"fig5.pdf")
+# plot n°6: Association between the state sequences and the selected covariate
+plot.filename <- file.path(data.folder,"fig06.pdf")
 gdiff <- seqdiff(traj.seq, group=perso[,"Sexe"], cmprange=c(0,5), 
 		seqdist.args=list(method="OM", indel=1, sm="TRATE"))
 pdf(file=plot.filename,bg="white",compress=FALSE)
 	plot(gdiff, main="Evolution of the pseudo R2 and L", stat=c("Pseudo R2", "Levene"), lwd=2)
 dev.off()
 
-# plot n°6: Evolution of the within-group discrepancy
-plot.filename <- file.path(data.folder,"fig6.pdf")
+# plot n°7: Evolution of the within-group discrepancy
+plot.filename <- file.path(data.folder,"fig07.pdf")
 pdf(file=plot.filename,bg="white",compress=FALSE)
 	plot(gdiff, main="Evolution of within-group and overall discrepancies", 
 			stat=c("discrepancy"), lwd=2, legend.pos="topright")
 dev.off()
 
 # plot n°7: Regression tree for the state sequences
-plot.filename <- file.path(data.folder,"fig7.png")
+plot.filename <- file.path(data.folder,"fig08.png")
 st <- seqtree(traj.seq ~ Sexe + `Groupe politique` + Profession, data=perso,
 		R=5000, diss=traj.dist, pval = 0.05)
-seqtreedisplay(st, filename=plot.filename, type="d", border=NA, gvpath='D:/Program Files (x86)/Graphviz2.38')
+seqtreedisplay(st, filename=plot.filename, type="d", border=NA, gvpath='C:/Program Files (x86)/Graphviz2.38')
 
 # plot n°8: Most frequent subsequences
-plot.filename <- file.path(data.folder,"fig8.pdf")
+plot.filename <- file.path(data.folder,"fig09.pdf")
 traj.seqe <- seqecreate(traj.seq, use.labels=FALSE)
 fsubseq <- seqefsub(traj.seqe, pmin.support=0.05)
 pdf(file=plot.filename,bg="white",compress=FALSE)
@@ -142,7 +148,7 @@ pdf(file=plot.filename,bg="white",compress=FALSE)
 dev.off()
 
 # plot n°9: Subsequences of transitions in each cluster
-plot.filename <- file.path(data.folder,"fig9.pdf")
+plot.filename <- file.path(data.folder,"fig10.pdf")
 discr <- seqecmpgroup(fsubseq, group=cl4.fac)
 pdf(file=plot.filename,bg="white",compress=FALSE)
 	plot(discr[1:6])
